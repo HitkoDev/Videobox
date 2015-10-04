e4f563a92c8d3928380d73b375282f3ab0ed59e4
root <development@hitko.si>
2015-10-04 16:41:20 -0400

    MODX package

---
+++ modx/_build/build.config.sample.php
@@ -0,0 +1,11 @@
+<?php
+define('MODX_BASE_PATH', '/srv/');			// MODX base path
+define('MODX_CORE_PATH', '/srv/core/');		// MODX core path
+define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
+define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
+define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
+ 
+define('MODX_BASE_URL', '/');				// URL of your MODX installation
+define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
+define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
+define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
--- modx/config.core.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('MODX_CORE_PATH', '/srv/core/');
-define('MODX_CONFIG_KEY', 'config');
-?>
