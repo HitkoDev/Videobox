8ffc2e4ebb4b89694f52e76c80f47a1898b193c8
root <development@hitko.si>
2015-10-19 15:07:07 -0400

    Properties

--- modx/_build/data/properties/properties.soundcloud.php
+++ modx/_build/data/properties/properties.soundcloud.php
@@ -1,3 +1,3 @@
 <?php
-$properties = json_decode('[{"name":"scVisual","desc":"","xtype":"combo-boolean","options":[],"value":true,"lexicon":"","overridden":false,"desc_trans":"","area":"","area_trans":"","menu":null}]', true);
+$properties = json_decode('[{"name":"scVisual","desc":"Turn visual player on (better for large players) or off (better for small players)","xtype":"combo-boolean","options":[],"value":true,"lexicon":"","overridden":false,"desc_trans":"Turn visual player on (better for large players) or off (better for small players)","area":"","area_trans":"","menu":null}]', true);
 return $properties;
