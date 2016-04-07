aa64732e1fdaa5a53125d69008b8c0ddbf429210
root <development@hitko.si>
2016-04-07 18:43:14 -0400

    Remove license header from overrides

--- gulpfile.js
+++ gulpfile.js
@@ -98,6 +98,7 @@ gulp.task('overrides', function() {
         .pipe(replace(/\/\*[^]*?\*\//ig, ''))
         .pipe(replace(/[\w\-]+\s*\:((?!(\$primary\-color|\$primary\-light|\:))[^])*?\;/igm, ''))
         .pipe(addsrc('./src/sass/videobox.scss'))
+        .pipe(replace(/\/\*[^]*?\*\//ig, ''))
         .pipe(concat('overrides.scss'))
         .pipe(replace(/\@import [^\;\{]*?\;/ig, ''))
         .pipe(replace(/(\$\s*)*\$\s+/igm, ''))
