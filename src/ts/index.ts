0698f65ef51bfa303249263d6e1120fb2e2f84b7
Hitko Development <development@hitko.si>
2016-11-18 18:05:32 +0100

    Version 6.0.0-rc6

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,20 @@ import { VideoboxObj as Videobox } from './components/box'
 import { VbInlineObj as VbInline } from './components/inline'
 import { VbSlider } from './components/slider'
 
-Videobox.bind("a[rel^='videobox']", {
-    root: <HTMLElement>(document.querySelector(".mdl-layout.mdl-js-layout") || document.body)
-})
-VbInline.bind("a[rel^='vbinline']")
-VbSlider.bind(".vb_slider")
+function bindAll() {
+    Videobox.bind("a[rel^='videobox']", {
+        root: <HTMLElement>(document.querySelector(".mdl-layout.mdl-js-layout") || document.body)
+    })
+    VbInline.bind("a[rel^='vbinline']")
+    VbSlider.bind(".vb_slider")
+}
+
+if (document.readyState != 'loading')
+    bindAll()
+else
+    document.addEventListener('readystatechange', function ready() {
+        if (document.readyState != 'loading') {
+            document.removeEventListener('readystatechange', ready)
+            bindAll()
+        }
+    })
